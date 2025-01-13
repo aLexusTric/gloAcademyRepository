cf34a5fedf26311857cfb762dd32d6a9118592ae
Aleksii Kostiukov <oleksiikostiukov@gmail.com>
2025-01-13 09:25:05 +0300

    finished lesson02

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "First project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 42;
+let rollback = 23;
+let fullPrice = 6000;
+let adaptive = true;
 
 alert("Hello!");
 console.log("Hello from the console!");
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + "$");
+console.log("Стоимость разроботки сайта " + fullPrice + "$");
+screens.toLowerCase();
+console.log(screens.split(" "));
+brokerFeePercentage = fullPrice * (rollback / 100);
+console.log(brokerFeePercentage);
