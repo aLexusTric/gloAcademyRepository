d87436de300ddd09f03332f5ec01061d6d3e52e3
Aleksii Kostiukov <oleksiikostiukov@gmail.com>
2025-01-15 08:03:44 +0300

    changed lesson03

--- script.js
+++ script.js
@@ -1,48 +1,30 @@
 'use strict';
-let title = "First project";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 42;
-let rollback = 23;
-let fullPrice = 6000;
-let adaptive = true;
-
 alert("Hello!");
-console.log("Hello from the console!");
-console.log(typeof title, typeof fullPrice, typeof adaptive);
-console.log(screens.length);
-console.log("Стоимость верстки экранов " + screenPrice + "$");
-console.log("Стоимость разработки сайта " + fullPrice + "$");
-console.log(screens.toLowerCase(screens.split(",")));
 
-title = prompt("Как называется ваш проект?");
-screens = prompt('Какие типы экранов нужно разработать?', 'пример: "Простые, Сложные, Интерактивные " ');
-screenPrice = Number(prompt('Сколько будет стоить данная работа?'));
-adaptive = confirm("Нужен ли адаптив на сайте?");
-console.log(adaptive);
+let title = prompt("Как называется ваш проект?");
+let screens = prompt('Какие типы экранов нужно разработать?', 'пример: "Простые, Сложные, Интерактивные " ');
+let screenPrice = Number(prompt('Сколько будет стоить данная работа?'));
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let rollback = 23;
 
-let addService1;
-let addService2;
-let addServicePrice1;
-let addServicePrice2;
+let addService1 = prompt('Какой дополнительный тип услуги нужен?', "пример: Service 1, Service 2");
+let addServicePrice1 = Number(prompt('Сколько это будет стоить?'));
 
-addService1 = prompt('Какой дополнительный тип услуги нужен?', "пример: Service 1, Service 2");
-addServicePrice1 = Number(prompt('Сколько это будет стоить?'));
+let addService2 = prompt('Какой дополнительный тип услуги нужен?', "пример: Service 1, Service 2");
+let addServicePrice2 = Number(prompt('Сколько это будет стоить?'));
 
-addService2 = prompt('Какой дополнительный тип услуги нужен?', "пример: Service 1, Service 2");
-addServicePrice2 = Number(prompt('Сколько это будет стоить?'));
-
-fullPrice = Number(screenPrice) + Number(addServicePrice1) + Number(addServicePrice2);
+let fullPrice = Number(screenPrice) + Number(addServicePrice1) + Number(addServicePrice2);
 
 let brokerFeePercentage = fullPrice * (rollback / 100);
 console.log(brokerFeePercentage);
 
-let servicePercentPrice = fullPrice - brokerFeePercentage;
-console.log(Math.ceil(servicePercentPrice));
+let servicePercentPrice = Math.ceil(fullPrice - brokerFeePercentage);
+console.log(servicePercentPrice);
 
 if (fullPrice >= 30000) {
     console.log('Даём скидку 10%')
 }
-else if (fullPrice > 15000) {
+else if (fullPrice >= 15000) {
     console.log('Даём скидку 5%')
 }
 else if (fullPrice < 15000 && fullPrice > 0) {
@@ -51,3 +33,16 @@ else if (fullPrice < 15000 && fullPrice > 0) {
 else {
     console.log('Что-то пошло не так');
 }
+
+console.log("Hello from the console!");
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + "$");
+console.log("Стоимость разработки сайта " + fullPrice + "$");
+console.log(screens.toLowerCase(screens.split(",")));
+console.log("Hello from the console!");
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + "$");
+console.log("Стоимость разработки сайта " + fullPrice + "$");
+console.log(screens.toLowerCase(screens.split(",")));
