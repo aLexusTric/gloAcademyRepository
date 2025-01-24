4612bb5f029e748e29079ca7d7d41b1924646bda
Aleksii Kostiukov <oleksiikostiukov@gmail.com>
2025-01-24 00:30:37 -0500

    fixed lesson04

--- script.js
+++ script.js
@@ -12,47 +12,49 @@ let addServicePrice1 = Number(prompt('Сколько это будет стои�
 
 let addService2 = prompt('Какой дополнительный тип услуги нужен?', "пример: Service 1, Service 2");
 let addServicePrice2 = Number(prompt('Сколько это будет стоить?'));
+let allServicePrice;
+let fullPrice;
+let brokerFeePercentage;
+let servicePercentPrice;
 
-const getAllServicePrices = function (){
-    return addServicePrice1 + addServicePrice2;
-  }
-
-let allServicePrice = getAllServicePrices(); 
-let fullPrice = getFullPrice();
-
-let brokerFeePercentage = fullPrice * (rollback / 100);
-let servicePercentPrice = Math.ceil(fullPrice - brokerFeePercentage);
-
-function getRollbackMessage(){
-if (fullPrice >= 30000){
-    return 'Даём скидку 10%'
-}
-else if (fullPrice >= 15000) {
-    return 'Даём скидку 5%'
-}
-else if (fullPrice < 15000 && fullPrice > 0) {
-    return 'Скидка не предусмотренна';
-}
-else {
-    return 'Что-то пошло не так';
-}
-}
 const showTypeOf = function (relevant){
     return typeof(relevant);
 }
 
+const getAllServicePrices = function (){
+    return addServicePrice1 + addServicePrice2;
+  }
+allServicePrice = getAllServicePrices(); 
 
 function getFullPrice(){
   return screenPrice + allServicePrice;
 }
+fullPrice = getFullPrice();
+brokerFeePercentage = fullPrice * (rollback / 100);
+servicePercentPrice = Math.ceil(fullPrice - brokerFeePercentage);
 
-const getTitle = function(title) {
-    return title.trim().charAt(0).toUpperCase() + title.trim().slice(1).toLowerCase();
-}
+function getRollbackMessage(){
+    if (fullPrice >= 30000){
+        return 'Даём скидку 10%'
+    }
+    else if (fullPrice >= 15000) {
+        return 'Даём скидку 5%'
+    }
+    else if (fullPrice < 15000 && fullPrice > 0) {
+        return 'Скидка не предусмотренна';
+    }
+    else {
+        return 'Что-то пошло не так';
+    }
+    }
 
 const getServicePercentPrices = function(){
     let servicePercentPrice = Math.ceil(fullPrice - brokerFeePercentage);
     return servicePercentPrice;
+}
+
+const getTitle = function(title) {
+    return title.trim().charAt(0).toUpperCase() + title.trim().slice(1).toLowerCase();
 }
     
 console.log(showTypeOf(title));
